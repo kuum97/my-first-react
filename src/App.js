ac03c205e3cfc5d59c02751f7e5951e6b7394f26
kuum97 <kuum97@icloud.com>
2023-05-22 05:14:14 +0900

    publishing

--- src/App.js
+++ src/App.js
@@ -5,13 +5,10 @@ import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path={process.env.PUBLIC_URL + "/"} element={<Home />}></Route>
-        <Route
-          path={process.env.PUBLIC_URL + "/movie/:id"}
-          element={<Detail />}
-        ></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movie/:id" element={<Detail />}></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,31 +1,58 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styles from "./Detail.module.css";
 
 function Detail() {
-  const [movie, setMovie] = useState("");
+  const [movie, setMovie] = useState(null);
   const { id } = useParams();
+
   const getMovie = async () => {
-    const json = await (
-      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
-    ).json();
-    setMovie(json.data.movie);
-    console.log(json.data.movie);
+    try {
+      const response = await fetch(
+        `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
+      );
+      const json = await response.json();
+      if (json.status === "ok") {
+        setMovie(json.data.movie);
+        console.log(json.data.movie);
+      } else {
+        console.error("Invalid API response:", json);
+      }
+    } catch (error) {
+      console.error("Error fetching movie:", error);
+    }
   };
+
   useEffect(() => {
     getMovie();
   });
+
+  if (!movie) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
-    <div>
-      <img src={movie.large_cover_image} alt={movie.title} />
-      <h1>{movie.title}</h1>
-      <span>{movie.year}</span>
-      <div>Rating: {movie.rating}</div>
-      <p>{movie.description_full}</p>
-      <ul>
-        {movie.genres.map((g) => (
-          <li key={g}>{g}</li>
-        ))}
-      </ul>
+    <div className={styles.container}>
+      <img
+        className={styles.movie_cover}
+        src={movie.large_cover_image}
+        alt={movie.title}
+      />
+      <div className={styles.movie_detail}>
+        <h1>{movie.title}</h1>
+        <span>Year : {movie.year}</span>
+        <span>Rating : {movie.rating}</span>
+        <p>
+          {movie.description_full > 1500
+            ? `${movie.description_full.slice(0, 1500)}...`
+            : movie.description_full}
+        </p>
+        <ul>
+          {movie.genres.map((genre) => (
+            <li key={genre}>{genre}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
